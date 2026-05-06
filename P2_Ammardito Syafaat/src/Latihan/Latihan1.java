40179784bdda46321883b516a7ae8b9fe1c54534
Ammardito Syafaat <ammarditosyafaatt@gmail.com>
2026-05-06 17:55:30 +0700

    Add files via upload

---
+++ P2_Ammardito Syafaat/src/Latihan/Latihan1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Latihan;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ADVAN
+ */
+public class Latihan1 {
+    public static void main (String arg[]) {
+        Scanner input = new Scanner (System.in);
+        String nama;
+        System.out.print("Input Nama: ");
+        nama=input.nextLine();
+        System.out.println("Nama:" + nama); 
+    }
+}
---
+++ P2_Ammardito Syafaat/src/Latihan/Latihan2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Latihan;
+
+/**
+ *
+ * @author ADVAN
+ */
+
+public class Latihan2 {
+    public static void main(String[]args){
+        
+        int nilaiSiswa = 90;
+        int NilaiSiswa = 100;
+        
+        boolean result = true;
+        char option ='C';
+        double grade = 0.0;
+        
+        System.out.println("Hasil:"+result);
+        System.out.println("Opsi:"+option);
+        System.out.println("Nilai awal:"+grade);
+        
+    }
+}
